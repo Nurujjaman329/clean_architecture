42dc431ff58be6a207ccf5e9459d2795d33f7ec1
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-11-09 16:45:48 +0600

    injection_container Bloc part

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:clean_architecture/features/daily_news/data/data_sources/remote/
 import 'package:clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 import 'package:clean_architecture/features/daily_news/domain/usecase/get_article.dart';
+import 'package:clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -30,6 +31,14 @@ Future<void> intializeDependencies() async {
 
   sl.registerSingleton<GetArticleUseCase>(
     GetArticleUseCase(
+      sl(),
+    ),
+  );
+
+  //Blocs
+
+  sl.registerFactory<RemoteArticlesBloc>(
+    () => RemoteArticlesBloc(
       sl(),
     ),
   );
